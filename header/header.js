0a8663b0ac013c6a2f67361fa453178a3f6bead9
sagarcia <sagarcia@scl.edu.gtgit config --global user.email sagarcia@scl.edu.gt>
2024-05-24 16:59:11 -0600

    headergenesis

---
+++ header/header.js
@@ -0,0 +1,10 @@
+let sof_hder = document.querySelector(".header");
+
+sof_hder.innerHTML = `
+
+    <h1 class ="inicio">Bienvenidos</h1>
+    <div class ="logo">
+    <img src="https://elegantquill.com/cdn/shop/products/business-logo-design-GS-initials.png?v=1670269433" alt="">
+    </div>
+
+`;
